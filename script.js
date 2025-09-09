3c405362bfb267e8c8cbbcff561f2697ec4f512d
graziyye <156468604+graziyye@users.noreply.github.com>
2025-09-09 12:12:37 -0300

    mostrar classificação

--- script.js
+++ script.js
@@ -9,13 +9,22 @@ function imc(){
 
     const valorIMC = (peso/(altura*altura)).toFixed(1);
 
-    resultado.textContent = `${nome} seu IMC é ${valorIMC}!`;
+
+    let classificacao = "";
+
+    if (valorIMC < 18.5){
+        classificacao = "Abaixo do peso";
+    }else if (valorIMC < 25){
+        classificacao = "Peso normal";
+    }else if (valorIMC < 30){
+        classificacao = "Levemente acima do peso";
+    }else{
+        classificacao="Obesidade";
+    }
+
+    resultado.textContent = `${nome} , seu IMC é ${valorIMC}. Portanto sua classificação é ${classificacao}`;
 }
 
-if(nome !="" && altura !=""){
-    const valorIMC = (peso/(altura*altura)).toFixed(1);
-    let classificacao="";
-    
-}
+
 
 calcular.addEventListener('click',imc);
